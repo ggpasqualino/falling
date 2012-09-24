375939e76d4438e21c14724839a2a981ba893fa5
guilherme <guilherme@guilherme-VirtualBox.(none)>
2012-09-24 10:48:59 -0300

    Teste 12

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -35,3 +35,16 @@ def postQueda():
         pass
     bottle.redirect('/queda')
 
+@route('/login')
+def login_form():
+    return '''<form method="POST" action="/login">
+                <input name="name"     type="text" />
+                <input name="password" type="password" />
+                <input type="submit" />
+              </form>'''
+
+@route('/login', method='POST')
+def login_submit():
+    name     = request.forms.get('name')
+    password = request.forms.get('password')
+    return "<p>Your login was correct</p>"
