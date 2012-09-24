617001937cb086fa22520d929fa00a4cbb995a88
guilherme <guilherme@guilherme-VirtualBox.(none)>
2012-09-24 10:56:24 -0300

    Teste 13

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -32,19 +32,4 @@ def postQueda():
         quedas.append(Queda(msg, dataEnvio))
         return msg + " " + dataEnvio
     except:
-        pass
-    bottle.redirect('/queda')
-
-@route('/login')
-def login_form():
-    return '''<form method="POST" action="/login">
-                <input name="name"     type="text" />
-                <input name="password" type="password" />
-                <input type="submit" />
-              </form>'''
-
-@route('/login', method='POST')
-def login_submit():
-    name     = bottle.request.forms.get('name')
-    password = bottle.request.forms.get('password')
-    return "<p>Your login was correct</p>"
+        bottle.redirect('/queda')
