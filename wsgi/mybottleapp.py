d845fcecd49d2fb6999d78c2fa39df473cf97b3d
guilherme <guilherme@guilherme-VirtualBox.(none)>
2012-09-24 09:11:16 -0300

    Teste 8

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -18,7 +18,7 @@ class Queda:
         self.dataEnvio = dataEnvio
         self.dataRecebido = str(datetime.now())
 
-quedas = []
+quedas = [Queda("Teste", "24/09/2012 09:10")]
     
 @route('/queda')
 def getQueda():
