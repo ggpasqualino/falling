05498f59e76d4fef2ee2fdb30ba7f84184a977cc
guilherme <guilherme@guilherme-VirtualBox.(none)>
2012-09-25 10:29:42 -0300

    Teste 29

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -2,6 +2,7 @@
 from bottle import route, default_app, TEMPLATE_PATH, template
 import os
 import bottle
+import pymongo
 from datetime import datetime
 
 bottle.debug(True)
@@ -10,18 +11,18 @@ bottle.TEMPLATE_PATH.append(
   os.path.join(
     os.environ['OPENSHIFT_GEAR_DIR'],'runtime/repo/wsgi/views/'))
 
-application = default_app()
+mongo_con = pymongo.Connection(
+  os.environ['OPENSHIFT_NOSQL_DB_HOST'],
+  int(os.environ['OPENSHIFT_NOSQL_DB_PORT']))
 
-class Queda:
-    def __init__(self, msg, dataEnvio):
-        self.msg= msg
-        self.dataEnvio = dataEnvio
-        self.dataRecebido = datetime.utcnow().strftime("%Y/%m/%d %H:%M")
+mongo_db = mongo_con[os.environ['OPENSHIFT_APP_NAME']]
+mongo_db.authenticate(
+    os.environ['OPENSHIFT_NOSQL_DB_USERNAME'],
+    os.environ['OPENSHIFT_NOSQL_DB_PASSWORD'])
 
-quedas = []
-    
 @route('/queda')
 def getQueda():
+    quedas = list(mongo_db.quedas.find())
     return bottle.template('visual', quedas=quedas, get_url=application.get_url)
 
 @route('/queda', method='POST')
@@ -29,7 +30,9 @@ def postQueda():
     try:        
         msg = bottle.request.forms.get('msg')
         dataEnvio = bottle.request.forms.get('dataEnvio')
-        quedas.append(Queda(msg, dataEnvio))
+        nova_queda = {'msg':msg, 'dataEnvio':dataEnvio, 
+                                'dataRecebido':datetime.utcnow().strftime("%Y/%m/%d %H:%M")}
+        mongo_db.quedas.insert(nova_queda)
         return msg + " " + dataEnvio
     except:
         bottle.redirect('/queda')
@@ -38,3 +41,6 @@ def postQueda():
 def server_static(filename):
     root_dir = os.path.join(os.getcwd(), 'static')
     return static_file(filename, root=root_dir)
+
+
+application = default_app()
--- wsgi/views/visual.tpl
+++ wsgi/views/visual.tpl
@@ -3,9 +3,10 @@
         <title>Falling Detection</title>
         <!--Bootstrap-->
         <link rel="stylesheet" type="text/css" href= "{{ get_url('static', filename='bootstrap.css') }}">    
+	<meta http-equiv="refresh" content="60">
     </head>
     <body>    
-        <div class="span8">
+        <div class="container">
             <table class="table table-striped table-bordered">
             <thead>
                 <tr>
@@ -17,9 +18,9 @@
             <tbody>
                 %for queda in quedas:
                 <tr>
-                    <td>{{queda.dataEnvio}}</td>
-                    <td>{{queda.dataRecebido}}</td>
-                    <td><span class="label label-important">{{queda.msg}}</span></td>
+                    <td>{{queda.get('dataEnvio')}}</td>
+                    <td>{{queda.get('dataRecebido')}}</td>
+                    <td><span class="label label-important">{{queda.get('msg')}}</span></td>
                 </tr>
                 %end
                 </tbody>
